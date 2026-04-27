0d8748c20ee614f879d605bc173d6ff22d260561
Enes Aydın <enes.aydin@agu.edu.tr>
2026-04-27 14:32:10 +0300

    login / logout button style and theme button style

--- public/js/auth.js
+++ public/js/auth.js
@@ -176,15 +176,16 @@ function updateNavbar() {
         nameSpan.textContent = displayName;
 
         const logoutBtn = document.createElement('button');
-        logoutBtn.className = 'btn-logout';
+        logoutBtn.className = 'btn-logout Btn';
         logoutBtn.id = 'logoutBtn';
         logoutBtn.type = 'button';
-        logoutBtn.innerHTML = '<i data-lucide="log-out" class="icon"></i><span>Logout</span>';
+        logoutBtn.title = 'Logout';
+        logoutBtn.innerHTML = '<div class="sign"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg></div><div class="text">Logout</div>';
 
         userInfo.appendChild(nameSpan);
         userInfo.appendChild(logoutBtn);
 
-        const themeBtn = actionsDiv.querySelector('.theme-toggle');
+        const themeBtn = actionsDiv.querySelector('.switch');
         if (themeBtn) actionsDiv.insertBefore(userInfo, themeBtn);
         else actionsDiv.appendChild(userInfo);
 
